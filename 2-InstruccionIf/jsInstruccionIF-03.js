8df2523245f2743fbd4638781d02033596c2c3cb
Giselle Mereles <gismereles@gmail.com>
2021-07-12 20:00:56 -0300

    tercerinstruccion

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,21 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+	edad = txtIdEdad.value;
+	edad = parseInt(edad);
+	
+//tomo la edad  
+	if(edad>=18)
+{
 
+	alert("Ud es mayor de edad");
+
+
+}
+	else
+	{
+		alert("Ud no es mayor de edad");
+
+	}
+	
 }//FIN DE LA FUNCIÓN
