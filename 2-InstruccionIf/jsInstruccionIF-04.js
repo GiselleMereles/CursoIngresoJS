82f3519dd8ed540e85af41807c9dc567ec96bbeb
Giselle Mereles <gismereles@gmail.com>
2021-07-12 22:37:42 -0300

    instrucion 4

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,23 @@
 function mostrar()
 {
+	let edad;
+	edad = txtIdEdad.value;
+	edad = parseInt(edad);
+
+	if(edad>=13 || edad<=17)                                      
+	{
+		alert("Ud  es adolescente");
+
+	}
+	else
+	{
+		alert("Ud es adolescente");
+
+	}
+	
+		
 	//tomo la edad  
-	alert("ok");
+	
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,19 @@
 function mostrar()
 {
+	let edad;
+
+	edad = txtIdEdad.value;
+	edad = parseInt(edad);
+	if(edad<13 || edad>17)
+	{
+		alert("Ud no es adolescente.");
+	}
+	else
+	{
+		alert("Ud no es adolescente")
+	}	
 	//tomo la edad  
-	alert("ok");
+	
 
 }//FIN DE LA FUNCIÓN
+//Al ingresar una edad solo debemos informar si la persona NO es adolescente.
